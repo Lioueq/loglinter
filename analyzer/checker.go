2d3ad95a4b889c34fca58e8af1a08cf2f29be5d8
Lioueq <dtaraskaev@yandex.ru>
2026-03-11 22:37:47 +0300

    check logger methods

--- analyzer/checker.go
+++ analyzer/checker.go
@@ -2,7 +2,6 @@ package analyzer
 
 import (
 	"go/ast"
-	"go/token"
 
 	"golang.org/x/tools/go/analysis"
 )
@@ -10,16 +9,43 @@ import (
 func run(pass *analysis.Pass) (any, error) {
 	for _, file := range pass.Files {
 		ast.Inspect(file, func(n ast.Node) bool {
-			be, ok := n.(*ast.BinaryExpr)
+			ce, ok := n.(*ast.CallExpr)
 			if !ok {
 				return true
 			}
-			if be.Op == token.ADD {
-				pass.Reportf(be.Pos(), "addiction found")
+			sel, ok := ce.Fun.(*ast.SelectorExpr)
+			if !ok || !isLoggerMethod(sel.Sel.Name) {
 				return true
 			}
+			obj := pass.TypesInfo.ObjectOf(sel.Sel)
+			if obj == nil || obj.Pkg() == nil {
+				return true
+			}
+			path := obj.Pkg().Path()
+			if !isLoggerExpr(path) {
+				return true
+			}
+
 			return true
 		})
 	}
 	return nil, nil
+}
+
+func isLoggerExpr(path string) bool {
+	switch path {
+	case "log/slog", "go.uber.org/zap":
+		return true
+	default:
+		return false
+	}
+}
+
+func isLoggerMethod(name string) bool {
+	switch name {
+	case "Info", "Error", "Warn", "Debug":
+		return true
+	default:
+		return false
+	}
 }
